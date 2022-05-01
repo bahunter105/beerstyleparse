e9ed2a30bcb25be5db276e1a9a433e359d7c5f0d
Hunter Bourgeois <bahunter@gmail.com>
2022-05-01 15:49:08 +0200

    parsing coming along

--- beerstyleparse.rb
+++ beerstyleparse.rb
@@ -7,14 +7,46 @@ html_file = URI.open("../beerstyleparse.html").read
 
 doc = Nokogiri::HTML(html_file) # create a nokogiri doc based on that html
 
-elements = doc.search('#beer-styles-toc h2')
-elements.class
-p elements
+data_array = []
 
-# elements.each do |element|
-p elements[0].text.strip
+elements = doc.search('.beer-style-descriptors')
+y_count = 0
+x_count = 0
+# elements.class
+# p elements.text
+
+lis= elements[2].search('li')
+
+# p lis[7].children[0].text == "\n\t\t\t\t"
+
+lis.each do |li|
+
+  if li.children[0].text == "\n\t\t\t\t"
+    p li.children[1].text.strip
+    p li.children[2].text.strip
+  else
+    # p li.children[0]
+    p li.children[0].child.text.strip
+    # p li.children[1]
+
+    p li.children[1].text.strip
+  end
+end
+# p lis[0].children[0].child.text
+# p lis[0].children[1].text
+
+# elements.each do |beer_description|
+
+
+#   uls= beer_description.search('ul')
+#   uls= elements[0].search('ul li')
+
+#   uls.each do |ul|
+#     lis = ul.search('li')
+
+#     print element.text
+#   end
 # end
-
 # Saving to CSV
 # filepath = "data/beatles.csv"
 # CSV.open(filepath, "wb") do |csv|
