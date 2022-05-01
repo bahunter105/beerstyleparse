c8220182b2ee21f24a8ac201097c96128e14e129
Hunter Bourgeois <bahunter@gmail.com>
2022-05-01 16:33:03 +0200

    document completely working

--- beerstyleparse.rb
+++ beerstyleparse.rb
@@ -2,56 +2,49 @@ require "open-uri"
 require "nokogiri"
 require "csv"
 
-url =
-html_file = URI.open("../beerstyleparse.html").read
-
+html_file = URI.open("../beerstyleparse.html").read #read url file
 doc = Nokogiri::HTML(html_file) # create a nokogiri doc based on that html
 
 data_array = []
 
 elements = doc.search('.beer-style-descriptors')
+
+# variables to run through the array
 y_count = 0
 x_count = 0
-# elements.class
-# p elements.text
 
-lis= elements[2].search('li')
+elements.each do |beer_description|
 
-# p lis[7].children[0].text == "\n\t\t\t\t"
+  lis= beer_description.search('li')
 
-lis.each do |li|
+  sub_array = []
 
-  if li.children[0].text == "\n\t\t\t\t"
-    p li.children[1].text.strip
-    p li.children[2].text.strip
-  else
-    # p li.children[0]
-    p li.children[0].child.text.strip
-    # p li.children[1]
+  lis.each do |li|
+    if li.children[0].text == "\n\t\t\t\t"  # avoid nil in this specific case
+      # p li.children[1].text.strip
+      sub_array[x_count] = li.children[2].text.strip
+    else
+      # p li.children[0].child.text.strip
+      sub_array[x_count] = li.children[1].text.strip
+    end
+    x_count += 1
+  end
+  data_array[y_count] = sub_array
+  y_count += 1
+  x_count = 0
 
-    p li.children[1].text.strip
+end
+
+# print data_array
+
+#titles for the csv
+titles = ["Color:", "Clarity:", "Perceived Malt Aroma & Flavor:", "Perceived Hop Aroma & Flavor:", "Perceived Bitterness:", "Fermentation Characteristics:", "Body:", "Additional notes:", "Original Gravity (°Plato)", "Apparent Extract/Final Gravity (°Plato)", "Alcohol by Weight (Volume)", "Bitterness (IBU)", "Color SRM (EBC)"]
+
+# Saving to CSV
+filepath = "../beerstyleparse.csv"
+CSV.open(filepath, "wb") do |csv|
+  csv << titles
+  data_array.each do |row|
+    csv << row
   end
 end
-# p lis[0].children[0].child.text
-# p lis[0].children[1].text
-
-# elements.each do |beer_description|
-
-
-#   uls= beer_description.search('ul')
-#   uls= elements[0].search('ul li')
-
-#   uls.each do |ul|
-#     lis = ul.search('li')
-
-#     print element.text
-#   end
-# end
-# Saving to CSV
-# filepath = "data/beatles.csv"
-# CSV.open(filepath, "wb") do |csv|
-#   # csv << ["First Name", "Last Name", "Instrument"]
-#   csv << ["John", "Lennon", "Guitar"]
-#   csv << ["Paul", "McCartney", "Bass Guitar"]
-#   # ...
-# end
